72852317a41367a9a8419c5ee633950de171a167
JakeLAmbertWD <jakelambert1@hotmail.co.uk>
2024-07-04 13:43:37 +0100

    Create FullScreenCarousel component

---
+++ src/app/FullScreenCarousel.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import videoCamera from "/public/video-outside.jpg";
+import videoRed from "/public/video-red.jpg";
+
+function FullScreenCarousel() {
+  return (
+    <Image
+      src={videoCamera}
+      alt="Picture of the author"
+      sizes="100vw"
+      style={{
+        width: "100vw", // Adjust width to 100vw to ensure it scales with the viewport width
+        height: "100vh", // Set height to 100vh to always take up full screen height
+        objectFit: "cover", // Ensure the image covers the available space without distortion
+        objectPosition: "center", // Keep the image centered
+      }}
+    />
+  );
+}
+
+export default FullScreenCarousel;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,7 @@
 "use client";
 
-import Image from "next/image";
-import videoCamera from "/public/video-outside.jpg";
-import videoRed from "/public/video-red.jpg";
+import FullScreenCarousel from "./FullScreenCarousel";
 
 export default function Home() {
-  return (
-    <Image
-      src={videoCamera}
-      alt="Picture of the author"
-      sizes="100vw"
-      style={{
-        width: "100vw", // Adjust width to 100vw to ensure it scales with the viewport width
-        height: "100vh", // Set height to 100vh to always take up full screen height
-        objectFit: "cover", // Ensure the image covers the available space without distortion
-        objectPosition: "center", // Keep the image centered
-      }}
-    />
-  );
+  return <FullScreenCarousel />;
 }
