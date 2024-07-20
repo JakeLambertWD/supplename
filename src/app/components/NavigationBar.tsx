71cdde2de5e8bc7b63c9c01fd7fc901fbdc174fd
JakeLAmbertWD <jakelambert1@hotmail.co.uk>
2024-07-20 10:21:45 +0100

    Add 'use client' to use dropdown in NavBar

--- src/app/components/NavigationBar.tsx
+++ src/app/components/NavigationBar.tsx
@@ -1,4 +1,6 @@
-import { Flex, Menu, NavLink, Text } from "@mantine/core";
+"use client";
+
+import { Button, Flex, Menu, NavLink, Text } from "@mantine/core";
 import { navigationLinks } from "../utils/constants";
 import {
   IconBrandInstagram,
@@ -47,28 +49,28 @@ function NavigationBar() {
 
       <Flex fz="xl">
         {navigationLinks.map((link) => {
-          // if (link.label === "Filmmaking") {
-          //   return (
-          //     <Menu>
-          //       <Menu.Target>
-          //         <NavLink
-          //           className={classes.noHoverColor}
-          //           // href={link.href}
-          //           label={link.label}
-          //           rightSection={<IconChevronDown size="1rem" stroke={1.5} />}
-          //           h={40}
-          //           childrenOffset={28}
-          //         />
-          //       </Menu.Target>
+          if (link.label === "Filmmaking") {
+            return (
+              <Menu>
+                <Menu.Target>
+                  <NavLink
+                    className={classes.noHoverColor}
+                    // href={link.href}
+                    label={link.label}
+                    rightSection={<IconChevronDown size="1rem" stroke={1.5} />}
+                    h={40}
+                    childrenOffset={28}
+                  />
+                </Menu.Target>
 
-          //       <Menu.Dropdown>
-          //         <Menu.Item>Commercial</Menu.Item>
-          //         <Menu.Item>Documentary</Menu.Item>
-          //         <Menu.Item>Music Video</Menu.Item>
-          //       </Menu.Dropdown>
-          //     </Menu>
-          //   );
-          // }
+                <Menu.Dropdown>
+                  <Menu.Item>Commercial</Menu.Item>
+                  <Menu.Item>Documentary</Menu.Item>
+                  <Menu.Item>Music Video</Menu.Item>
+                </Menu.Dropdown>
+              </Menu>
+            );
+          }
           return (
             <NavLink
               className={classes.noHoverColor}
@@ -85,11 +87,11 @@ function NavigationBar() {
         })}
       </Flex>
 
-      {/* <Flex gap="sm">
-        <IconBrandInstagram strokeWidth={0.9} color="white" />
-        <IconBrandYoutube strokeWidth={0.9} />
-        <IconBrandTwitter strokeWidth={0.9} />
-      </Flex> */}
+      <Flex gap="sm">
+        <Button variant="outline" color="white" size="md">
+          Let's chat
+        </Button>
+      </Flex>
     </Flex>
   );
 }
