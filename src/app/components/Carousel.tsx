6c4651fe98f8c5b49fcc7b4ecde313102136dff0
JakeLAmbertWD <jakelambert1@hotmail.co.uk>
2024-07-20 16:47:23 +0100

    Parallax scrolling

--- src/app/components/Carousel.tsx
+++ src/app/components/Carousel.tsx
@@ -1,26 +1,33 @@
 import Image from "next/image";
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 import { Flex, Overlay, Stack, Text } from "@mantine/core";
 import { latestWork } from "../utils/constants";
+import { useRef } from "react";
 
 function FullScreenCarousel({ active }: { active: number }) {
   const getActiveWork = latestWork.filter((work, index) => index === active)[0];
   const image = getActiveWork?.image;
-
   const activeSlide = latestWork.filter((work, index) => index === active)[0];
+  const ref = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+  const backgroundY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
 
   return (
-    <Flex h="100vh" align="flex-end">
-      <Image
-        src={image}
+    <Flex h="100vh" align="flex-end" ref={ref}>
+      <motion.img
+        src={image.src}
         alt="Picture of the author"
-        sizes="100vw"
         style={{
           width: "100vw",
           height: "100vh",
           objectFit: "cover",
           position: "absolute",
           zIndex: -1,
+          y: backgroundY,
         }}
       />
       <Overlay
@@ -32,16 +39,16 @@ function FullScreenCarousel({ active }: { active: number }) {
       />
       <Stack c="white" gap={0} mb={70} ml={70}>
         <motion.p
-          initial={{ x: -500, opacity: 0 }}
-          whileInView={{ x: 0, opacity: 1 }}
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
           transition={{ duration: 0.5, delay: 0.5 }}
           style={{ fontSize: 35, fontWeight: 600, margin: 0 }}
         >
           {activeSlide?.title}
         </motion.p>
         <motion.p
-          initial={{ x: -500, opacity: 0 }}
-          whileInView={{ x: 0, opacity: 1 }}
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
           transition={{ duration: 0.5, delay: 1 }}
           style={{ fontSize: 25, margin: 0 }}
         >
