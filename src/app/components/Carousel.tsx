939584ef80f6245ffc9e578878eb17db6963be71
JakeLAmbertWD <jakelambert1@hotmail.co.uk>
2024-07-24 16:17:45 +0100

    Add more opacity to video background

--- src/app/components/Carousel.tsx
+++ src/app/components/Carousel.tsx
@@ -46,7 +46,7 @@ function FullScreenCarousel() {
         />
         <Overlay
           color="#0b0f19"
-          backgroundOpacity={0.4}
+          backgroundOpacity={0.6}
           pos="absolute"
           h={"100vh"}
           style={{ zIndex: 0 }}
