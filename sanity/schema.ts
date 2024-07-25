e107f07dabc7953f7b89144762c521c8e48b3946
JakeLAmbertWD <jakelambert1@hotmail.co.uk>
2024-07-25 17:32:33 +0100

    Add to array of schemas

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,7 +1,8 @@
 import { type SchemaTypeDefinition } from "sanity";
 
 import pageInfo from "./schemaTypes/pageInfo";
+import latestWork from "./schemaTypes/latestWork";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [pageInfo],
+  types: [pageInfo, latestWork],
 };
