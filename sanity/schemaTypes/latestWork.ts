df0693bd598ef9531b8fb8d423c9f54e06eebe56
JakeLAmbertWD <jakelambert1@hotmail.co.uk>
2024-07-25 17:32:23 +0100

    Latest work schema types

---
+++ sanity/schemaTypes/latestWork.ts
@@ -0,0 +1,40 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+  name: "latestWork",
+  title: "LatestWork",
+  type: "document",
+  fields: [
+    defineField({
+      name: "client",
+      title: "Client",
+      type: "string",
+    }),
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+    }),
+    defineField({
+      name: "youtubeID",
+      title: "YouTube ID",
+      type: "string",
+    }),
+    defineField({
+      name: "tileImage",
+      title: "Tile Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: "logo",
+      title: "Logo",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    }),
+  ],
+});
