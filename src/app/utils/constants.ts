1c3d610c765bfb19fe9b8eb4b95fa6ffde7234bd
JakeLAmbertWD <jakelambert1@hotmail.co.uk>
2024-08-24 08:25:05 +0100

    Clean up constants

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -1,16 +1,7 @@
-import workThumb1 from "/public/video-red.jpg";
-import workThumb2 from "/public/sparklers.jpg";
-import workThumb3 from "/public/festival.jpg";
-import workThumb4 from "/public/party.jpg";
-import workThumb5 from "/public/festival.jpg";
-import workThumb6 from "/public/party.jpg";
-import workThumb7 from "/public/sparklers.jpg";
-
 export const navigationLinks = [
   {
     href: "/works",
     label: "Works",
-    dropdown: true,
   },
   {
     href: "/bio",
@@ -20,69 +11,4 @@ export const navigationLinks = [
     href: "/awards",
     label: "Awards",
   },
-];
-
-export const filmmakingLinks = [
-  {
-    href: "/commercials",
-    label: "Commercials",
-  },
-  {
-    href: "/music-videos",
-    label: "Music Videos",
-  },
-  {
-    href: "/event-live",
-    label: "Event Live",
-  },
-  {
-    href: "/tv",
-    label: "TV",
-  },
-  {
-    href: "/film",
-    label: "Film",
-  },
-  {
-    href: "/directing",
-    label: "Directing",
-  },
-];
-
-export const latestWork = [
-  {
-    title: "Lucozade Bring the Energy",
-    description: "Ft. Jude Bellingham",
-    image: workThumb1,
-  },
-  {
-    title: "Honda HRV ",
-    description: "Stepping",
-    image: workThumb2,
-  },
-  {
-    title: "McDonalds",
-    description: "McNugget",
-    image: workThumb3,
-  },
-  {
-    title: "Chemical Brothers",
-    description: "Midnight Madness",
-    image: workThumb4,
-  },
-  // {
-  //   title: "Budweiser",
-  //   description: "Festival",
-  //   image: workThumb5,
-  // },
-  // {
-  //   title: "Eve Sleep",
-  //   description: "Sloth",
-  //   image: workThumb6,
-  // },
-  // {
-  //   title: "B&Q",
-  //   description: "Labelling Team",
-  //   image: workThumb7,
-  // },
 ];
