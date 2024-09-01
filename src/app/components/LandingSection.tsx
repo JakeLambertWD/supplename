54cace4a1819f27e41c1c54c871bb765b58a18e6
JakeLAmbertWD <jakelambert1@hotmail.co.uk>
2024-09-01 11:22:52 +0100

    Remove motion primitives

--- src/app/components/LandingSection.tsx
+++ src/app/components/LandingSection.tsx
@@ -4,7 +4,6 @@ import { theme } from "../utils/theme";
 import { useDisclosure } from "@mantine/hooks";
 import classes from "../components/css/Project.module.css";
 import YouTube from "react-youtube";
-import { TextEffect } from "./animations/TextEffect";
 
 export default function LandingSection({ jobTitle }: { jobTitle: string }) {
   const [opened, { open, close }] = useDisclosure(false);
@@ -36,9 +35,9 @@ export default function LandingSection({ jobTitle }: { jobTitle: string }) {
           w={{ base: "85%" }}
           style={{ zIndex: 7 }}
         >
-          <TextEffect per="word" preset="blur" className={classes.jobTitle}>
-            CHOREOGRAPHER - DIRECTOR
-          </TextEffect>
+          <Text fz={14} mb={5}>
+            {jobTitle}
+          </Text>
           <Group>
             <Button
               color={theme.colors?.primary?.[1]}
