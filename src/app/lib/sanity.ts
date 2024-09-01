95dc472d107ff6eb7ebf50a093b96faeeae2d923
JakeLAmbertWD <jakelambert1@hotmail.co.uk>
2024-09-01 15:53:11 +0100

    Edit query to order items on Awards and Home page

--- src/app/lib/sanity.ts
+++ src/app/lib/sanity.ts
@@ -35,14 +35,14 @@ export async function getWorks() {
 
 export async function getAwards() {
   const awards = await client.fetch(
-    '*[_type == "awardsPage"] { name, order, work->{ client, description, overview, "videoURL": video.asset->url, youtubeID, "tileImage": tileImage.asset->url, projectGenre->{ name }, award } }'
+    '*[_type == "awardsPage"] | order(order asc) { name, order, work->{ client, description, overview, "videoURL": video.asset->url, youtubeID, "tileImage": tileImage.asset->url, projectGenre->{ name }, award } }'
   );
   return awards;
 }
 
 export async function getHomePage() {
   const homePage = await client.fetch(
-    '*[_type == "homePage"] { name, order, featuredWork->{ client, description, overview, "videoURL": video.asset->url, youtubeID, "tileImage": tileImage.asset->url, projectGenre->{ name }, award } }'
+    '*[_type == "homePage"] | order(order asc) { name, order, featuredWork->{ client, description, overview, "videoURL": video.asset->url, youtubeID, "tileImage": tileImage.asset->url, projectGenre->{ name }, award } }'
   );
   return homePage;
 }
