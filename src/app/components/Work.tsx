c594791857d9bbbb9b2bc1b4f18c07c0e9be7c1b
JakeLAmbertWD <jakelambert1@hotmail.co.uk>
2024-09-14 09:28:41 +0100

    Remove HoverCard on small screens

--- src/app/components/Work.tsx
+++ src/app/components/Work.tsx
@@ -9,6 +9,8 @@ import { useRouter } from "next/navigation";
 import { useFormattedDescription } from "../../../hooks/useFormattedDescription";
 import { theme } from "../utils/theme";
 import { WorkProps } from "../utils/typings";
+import { SM } from "../utils/constants";
+import { useMediaQuery } from "@mantine/hooks";
 
 interface ProjectProps {
   work: WorkProps;
@@ -17,152 +19,178 @@ interface ProjectProps {
 function Work({ work }: ProjectProps) {
   const router = useRouter();
   const workDescription = useFormattedDescription(work.description);
+  const isSM = useMediaQuery(`(max-width: ${SM})`);
 
   return (
     <>
-      <HoverCard
-        width={280}
-        shadow="xl"
-        openDelay={400}
-        offset={-320}
-        position="bottom"
-        radius={4}
-        transitionProps={{ duration: 500, transition: "pop" }}
-      >
-        <HoverCard.Target>
-          <Card
-            shadow="xl"
-            p={0}
-            radius={0}
-            w={{ base: "100%", sm: 300 }}
-            h={{ base: "auto", sm: 300 }}
-            pos="relative"
-            onClick={() => {
-              router.push(`/works/${workDescription}`);
-            }}
-            style={{ cursor: "pointer" }}
-          >
-            <motion.img
-              src={work.tileImage}
-              alt="Image"
-              style={{
-                objectFit: "cover",
-                width: "100%",
-                height: "100%",
-              }}
-            />
-          </Card>
-        </HoverCard.Target>
-        <HoverCard.Dropdown
-          bg={theme?.colors?.primary?.[8]}
-          w={350}
-          h={350}
+      {isSM ? (
+        <Card
+          shadow="xl"
           p={0}
+          radius={0}
+          w={{ base: "100%", sm: 300 }}
+          h={{ base: "auto", sm: 300 }}
+          pos="relative"
           onClick={() => {
             router.push(`/works/${workDescription}`);
           }}
-          style={{ border: "none", zIndex: 10, cursor: "pointer" }}
+          style={{ cursor: "pointer" }}
         >
-          {work.videoURL ? (
-            <video
-              autoPlay
-              loop
-              muted
-              playsInline
-              preload="auto"
-              style={{
-                width: "100%",
+          <motion.img
+            src={work.tileImage}
+            alt="Image"
+            style={{
+              objectFit: "cover",
+              width: "100%",
+              height: "100%",
+            }}
+          />
+        </Card>
+      ) : (
+        <HoverCard
+          width={280}
+          shadow="xl"
+          openDelay={400}
+          offset={-320}
+          position="bottom"
+          radius={4}
+          transitionProps={{ duration: 500, transition: "pop" }}
+        >
+          <HoverCard.Target>
+            <Card
+              shadow="xl"
+              p={0}
+              radius={0}
+              w={{ base: "100%", sm: 300 }}
+              h={{ base: "auto", sm: 300 }}
+              pos="relative"
+              onClick={() => {
+                router.push(`/works/${workDescription}`);
               }}
+              style={{ cursor: "pointer" }}
             >
-              <source src={work.videoURL} type="video/mp4" />
-            </video>
-          ) : (
-            <motion.img
-              src={work.tileImage}
-              alt="Image"
-              style={{
-                objectFit: "cover",
-                objectPosition: "top",
-                width: 350,
-                height: 200,
-              }}
-            />
-          )}
-
-          <Stack gap={0} px="lg">
-            <Group justify="space-between">
-              <Text c={theme?.colors?.primary?.[5]}>{work.client}</Text>
-              <IconPlayerPlayFilled
-                size={25}
-                color={theme?.colors?.primary?.[5]}
-                style={{ marginTop: 10 }}
+              <motion.img
+                src={work.tileImage}
+                alt="Image"
+                style={{
+                  objectFit: "cover",
+                  width: "100%",
+                  height: "100%",
+                }}
               />
-            </Group>
-            <Group
-              wrap="nowrap"
-              style={{ overflowX: "auto", scrollbarWidth: "none" }}
-            >
-              <Text
-                fz="xl"
-                c={theme?.colors?.primary?.[5]}
-                mb="xs"
-                style={{ whiteSpace: "nowrap" }}
+            </Card>
+          </HoverCard.Target>
+          <HoverCard.Dropdown
+            bg={theme?.colors?.primary?.[8]}
+            w={350}
+            h={350}
+            p={0}
+            onClick={() => {
+              router.push(`/works/${workDescription}`);
+            }}
+            style={{ border: "none", zIndex: 10, cursor: "pointer" }}
+          >
+            {work.videoURL ? (
+              <video
+                autoPlay
+                loop
+                muted
+                playsInline
+                preload="auto"
+                style={{
+                  width: "100%",
+                }}
               >
-                {work.description}
-              </Text>
-              <IconBadge4k
-                size={25}
-                color="white"
-                strokeWidth={0.6}
-                style={{ marginBottom: 8 }}
+                <source src={work.videoURL} type="video/mp4" />
+              </video>
+            ) : (
+              <motion.img
+                src={work.tileImage}
+                alt="Image"
+                style={{
+                  objectFit: "cover",
+                  objectPosition: "top",
+                  width: 350,
+                  height: 200,
+                }}
               />
-            </Group>
+            )}
 
-            {
-              // @ts-ignore
-              work.award && (
-                <Group>
-                  <Text
-                    c="orange"
-                    fz="xs"
-                    style={{ whiteSpace: "nowrap", fontStyle: "italic" }}
-                  >
-                    {work.award[0].title}
-                  </Text>
-                  <IconTrophy
-                    size={23}
-                    strokeWidth={0.8}
-                    color="orange"
-                    style={{ marginTop: -5 }}
-                  />
-                </Group>
-              )
-            }
+            <Stack gap={0} px="lg">
+              <Group justify="space-between">
+                <Text c={theme?.colors?.primary?.[5]}>{work.client}</Text>
+                <IconPlayerPlayFilled
+                  size={25}
+                  color={theme?.colors?.primary?.[5]}
+                  style={{ marginTop: 10 }}
+                />
+              </Group>
+              <Group
+                wrap="nowrap"
+                style={{ overflowX: "auto", scrollbarWidth: "none" }}
+              >
+                <Text
+                  fz="xl"
+                  c={theme?.colors?.primary?.[5]}
+                  mb="xs"
+                  style={{ whiteSpace: "nowrap" }}
+                >
+                  {work.description}
+                </Text>
+                <IconBadge4k
+                  size={25}
+                  color="white"
+                  strokeWidth={0.6}
+                  style={{ marginBottom: 8 }}
+                />
+              </Group>
 
-            <Flex
-              gap={10}
-              mt="xs"
-              w={310}
-              wrap="nowrap"
-              style={{ overflowX: "auto", scrollbarWidth: "none" }}
-            >
-              {work.movementGenres.map((genre: any, index: number) => {
-                return (
-                  <Group key={index} gap={10} wrap="nowrap">
-                    <Text fz="md" c="white" style={{ whiteSpace: "nowrap" }}>
-                      {genre.name}
+              {
+                // @ts-ignore
+                work.award && (
+                  <Group>
+                    <Text
+                      c="orange"
+                      fz="xs"
+                      style={{ whiteSpace: "nowrap", fontStyle: "italic" }}
+                    >
+                      {work.award[0].title}
                     </Text>
-
-                    {index !== work.movementGenres.length - 1 && (
-                      <Text c={theme?.colors?.primary?.[5]}>-</Text>
-                    )}
+                    <IconTrophy
+                      size={23}
+                      strokeWidth={0.8}
+                      color="orange"
+                      style={{ marginTop: -5 }}
+                    />
                   </Group>
-                );
-              })}
-            </Flex>
-          </Stack>
-        </HoverCard.Dropdown>
-      </HoverCard>
+                )
+              }
+
+              <Flex
+                gap={10}
+                mt="xs"
+                w={310}
+                wrap="nowrap"
+                style={{ overflowX: "auto", scrollbarWidth: "none" }}
+              >
+                {work.movementGenres.map((genre: any, index: number) => {
+                  return (
+                    <Group key={index} gap={10} wrap="nowrap">
+                      <Text fz="md" c="white" style={{ whiteSpace: "nowrap" }}>
+                        {genre.name}
+                      </Text>
+
+                      {index !== work.movementGenres.length - 1 && (
+                        <Text c={theme?.colors?.primary?.[5]}>-</Text>
+                      )}
+                    </Group>
+                  );
+                })}
+              </Flex>
+            </Stack>
+          </HoverCard.Dropdown>
+        </HoverCard>
+      )}
     </>
   );
 }
