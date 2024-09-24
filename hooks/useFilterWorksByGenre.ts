b4be114f8a0a7b8140b5ab2e1760d9acf5f27a68
JakeLAmbertWD <jakelambert1@hotmail.co.uk>
2024-09-24 19:58:45 +0100

    Modify useFilterWorksByGenre to filter works by awards

--- hooks/useFilterWorksByGenre.ts
+++ hooks/useFilterWorksByGenre.ts
@@ -1,17 +1,28 @@
 import { useMemo } from "react";
 import { GenreProps, WorkProps } from "../src/app/utils/typings";
+import { useGetAwards } from "./getAwards";
 
 export const useFilterWorksByGenre = (
   genres: GenreProps[],
   works: WorkProps[],
   activeGenreTab: number
 ) => {
+  const { awards } = useGetAwards();
+
   return useMemo(() => {
     const activeGenre = genres[activeGenreTab];
+
     if (!activeGenre) return [];
 
+    // get all works that have won an award
     if (activeGenre.name === "Awards") {
-      return works.filter((work) => work.award);
+      const awardWorks = awards
+        .map((award) =>
+          works.find((work) => work.description === award.work.description)
+        )
+        .filter((work) => work !== undefined);
+
+      return awardWorks;
     }
 
     return works.filter((work) => work.projectGenre.name === activeGenre.name);
