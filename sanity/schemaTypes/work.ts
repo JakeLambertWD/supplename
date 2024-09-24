777a96f2db77e2097d6e9f3d4ede0618bd4d46b6
JakeLAmbertWD <jakelambert1@hotmail.co.uk>
2024-09-24 19:57:01 +0100

    Remove award from Schema and from inside the Work schema

--- sanity/schemaTypes/work.ts
+++ sanity/schemaTypes/work.ts
@@ -72,12 +72,6 @@ export default defineType({
       of: [{ type: "workImage" }],
     }),
     defineField({
-      name: "award",
-      title: "Award",
-      type: "array",
-      of: [{ type: "awards" }],
-    }),
-    defineField({
       name: "startTime",
       title: "Start Time",
       type: "number",
