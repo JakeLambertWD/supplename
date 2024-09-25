6d49c7f55b32b805aea35b5ba37bdac9573a517b
JakeLAmbertWD <jakelambert1@hotmail.co.uk>
2024-09-25 20:12:38 +0100

    Add date orders to the Work Schema

--- sanity/schemaTypes/work.ts
+++ sanity/schemaTypes/work.ts
@@ -6,6 +6,11 @@ export default defineType({
   type: "document",
   fields: [
     defineField({
+      name: "order",
+      title: "Order",
+      type: "date",
+    }),
+    defineField({
       name: "client",
       title: "Client",
       type: "string",
--- src/app/lib/sanity.ts
+++ src/app/lib/sanity.ts
@@ -24,7 +24,7 @@ export async function getAwards() {
 
 export async function getWorks() {
   const works = await client.fetch(
-    '*[_type == "work" && hide != true] { client, hide, description, overview, "videoURL": video.asset->url, workImages[] { asset->{ url }, alt }, team[]->{ name, role }, movementGenres[]->{ name }, youtubeID, "tileImage": tileImage.asset->url, projectGenre->{ name }, startTime }'
+    '*[_type == "work" && hide != true] | order(coalesce(order, 999999999) desc) { client, order, hide, description, overview, "videoURL": video.asset->url, workImages[] { asset->{ url }, alt }, team[]->{ name, role }, movementGenres[]->{ name }, youtubeID, "tileImage": tileImage.asset->url, projectGenre->{ name }, startTime }'
   );
   return works;
 }
