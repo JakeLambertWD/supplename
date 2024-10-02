84909fc7cff73490cb8b339566b2e6e9e72adddc
JakeLAmbertWD <jakelambert1@hotmail.co.uk>
2024-10-02 20:46:00 +0100

    Extract code out to Award component

--- src/app/awards/page.tsx
+++ src/app/awards/page.tsx
@@ -1,35 +1,13 @@
 "use client";
 
-import { Button, Center, Flex, Grid, Space, Stack, Text } from "@mantine/core";
+import { Space, Stack, Text } from "@mantine/core";
 import NavigationBar from "../components/NavigationBar";
-import { motion } from "framer-motion";
-import { theme } from "../utils/theme";
 import { FooterSocial } from "../components/Footer";
-import { useEffect, useState } from "react";
-import { getAwards } from "../lib/sanity";
-import { AwardPageProps } from "../utils/typings";
-import { useRouter } from "next/navigation";
-import { activeGenreTabState } from "../../../atoms/atoms";
-import { useRecoilState } from "recoil";
-import { useMediaQuery } from "@mantine/hooks";
-import { SM } from "../utils/constants";
-import { useFormattedDescription } from "../../../hooks/useFormattedDescription";
+import { useGetAwards } from "../../../hooks/getAwards";
+import Award from "../components/Award";
 
 function page() {
-  const isSM = useMediaQuery(`(max-width: ${SM})`);
-  const [awards, setAwards] = useState<AwardPageProps[]>([]);
-  const router = useRouter();
-
-  const [activeGenreTab, setActiveGenreTab] =
-    useRecoilState(activeGenreTabState);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const awardsData = await getAwards();
-      setAwards(awardsData);
-    };
-    fetchData();
-  }, []);
+  const { awards } = useGetAwards();
 
   return (
     <>
@@ -42,88 +20,8 @@ function page() {
       <Stack gap={0}>
         {awards?.map((award, index) => {
           const isOdd = index % 2 === 0;
-          const formattedDescription = useFormattedDescription(
-            award.work.description
-          );
 
-          return (
-            <>
-              <Grid
-                gutter={0}
-                style={{ cursor: "pointer" }}
-                onClick={() => {
-                  router.push(`/works/${formattedDescription}`);
-                  setActiveGenreTab(5);
-                }}
-              >
-                <Grid.Col span={{ sm: 6 }} order={{ sm: isOdd ? 1 : 2 }}>
-                  <Flex pos="relative" align="center" justify="center">
-                    <motion.video
-                      key={award?.work?.videoURL}
-                      autoPlay
-                      loop
-                      muted
-                      playsInline
-                      style={{
-                        width: "100%",
-                        height: "500px",
-                        objectFit: "cover",
-                        zIndex: -1,
-                      }}
-                    >
-                      <source src={award?.work?.videoURL} type="video/mp4" />
-                      Your browser does not support the video tag.
-                    </motion.video>
-
-                    <Text
-                      c="white"
-                      fz="xl"
-                      fw={800}
-                      ta="center"
-                      px="lg"
-                      pos="absolute"
-                    >
-                      {award.name}
-                    </Text>
-                  </Flex>
-                </Grid.Col>
-
-                <Grid.Col span={{ sm: 6 }} order={{ sm: isOdd ? 2 : 1 }}>
-                  <Center
-                    p={{ base: 30, xs: 40, sm: 80, md: 90, lg: 110, xl: 130 }}
-                    h="100%"
-                    c="white"
-                  >
-                    <Stack
-                      w="100%"
-                      align={
-                        !isSM ? (isOdd ? "flex-end" : "flex-start") : "center"
-                      }
-                      ta={{ sm: "right" }}
-                    >
-                      <Text ta={isSM ? "center" : "match-parent"}>
-                        {award.name}
-                      </Text>
-
-                      <Button
-                        color={theme.colors?.primary?.[1]}
-                        size="md"
-                        w="fit-content"
-                        onClick={() => {
-                          router.push(`/works/${formattedDescription}`);
-                          setActiveGenreTab(5);
-                        }}
-                      >
-                        Go To Work
-                      </Button>
-                      <Text>{award.work.client}</Text>
-                      <Text>{award.work.description}</Text>
-                    </Stack>
-                  </Center>
-                </Grid.Col>
-              </Grid>
-            </>
-          );
+          return <Award award={award} isOdd={isOdd} />;
         })}
       </Stack>
 
---
+++ src/app/components/Award.tsx
@@ -0,0 +1,93 @@
+import { Button, Center, Flex, Grid, Stack, Text } from "@mantine/core";
+import { useRouter } from "next/navigation";
+import { motion } from "framer-motion";
+import { useMediaQuery } from "@mantine/hooks";
+import { SM } from "../utils/constants";
+import { theme } from "../utils/theme";
+import { useFormattedDescription } from "../../../hooks/useFormattedDescription";
+import { useRecoilState } from "recoil";
+import { activeGenreTabState } from "../../../atoms/atoms";
+
+function Award({ award, isOdd }: any) {
+  const router = useRouter();
+  const isSM = useMediaQuery(`(max-width: ${SM})`);
+
+  const formattedDescription = useFormattedDescription(award.work.description);
+  const [activeGenreTab, setActiveGenreTab] =
+    useRecoilState(activeGenreTabState);
+
+  // const videoRef = useRef<HTMLVideoElement>(null);
+  // useEffect(() => {
+  //   if (videoRef.current) {
+  //     videoRef.current.currentTime = featuredWork.startTime; // Set the start time in seconds
+  //   }
+  // }, []);
+
+  return (
+    <Grid
+      gutter={0}
+      style={{ cursor: "pointer" }}
+      onClick={() => {
+        router.push(`/works/${formattedDescription}`);
+        setActiveGenreTab(5);
+      }}
+    >
+      <Grid.Col span={{ sm: 6 }} order={{ sm: isOdd ? 1 : 2 }}>
+        <Flex pos="relative" align="center" justify="center">
+          <motion.video
+            key={award?.work?.videoURL}
+            autoPlay
+            loop
+            muted
+            playsInline
+            style={{
+              width: "100%",
+              height: "500px",
+              objectFit: "cover",
+              zIndex: -1,
+            }}
+          >
+            <source src={award?.work?.videoURL} type="video/mp4" />
+            Your browser does not support the video tag.
+          </motion.video>
+
+          <Text c="white" fz="xl" fw={800} ta="center" px="lg" pos="absolute">
+            {award.name}
+          </Text>
+        </Flex>
+      </Grid.Col>
+
+      <Grid.Col span={{ sm: 6 }} order={{ sm: isOdd ? 2 : 1 }}>
+        <Center
+          p={{ base: 30, xs: 40, sm: 80, md: 90, lg: 110, xl: 130 }}
+          h="100%"
+          c="white"
+        >
+          <Stack
+            w="100%"
+            align={!isSM ? (isOdd ? "flex-end" : "flex-start") : "center"}
+            ta={{ sm: "right" }}
+          >
+            <Text ta={isSM ? "center" : "match-parent"}>{award.name}</Text>
+
+            <Button
+              color={theme.colors?.primary?.[1]}
+              size="md"
+              w="fit-content"
+              onClick={() => {
+                router.push(`/works/${formattedDescription}`);
+                setActiveGenreTab(5);
+              }}
+            >
+              Go To Work
+            </Button>
+            <Text>{award?.work?.client}</Text>
+            <Text>{award?.work?.description}</Text>
+          </Stack>
+        </Center>
+      </Grid.Col>
+    </Grid>
+  );
+}
+
+export default Award;
