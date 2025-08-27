0118fdd8ce70b04f27a89af49db55bb091381129
Jake Lambert <jakelambert1@icloud.com>
2025-08-27 20:57:55 +0100

    Fix build

--- src/app/utils/typings.ts
+++ src/app/utils/typings.ts
@@ -1,49 +1,50 @@
 export interface HomePageWorkProps {
-  name: string;
-  order: number;
-  featuredWork: WorkProps;
+	name: string;
+	order: number;
+	featuredWork: WorkProps;
 }
 
 export interface WorkProps {
-  client: string;
-  description: string;
-  overview: string;
-  projectGenre: GenreProps;
-  tileImage: string;
-  videoURL: string;
-  videoPreview: string;
-  team: TeamProps[];
-  movementGenres: MovementGenreProps[];
-  workImages: string[];
+	client: string;
+	description: string;
+	overview: string;
+	projectGenre: GenreProps;
+	tileImage: string;
+	videoURL: string;
+	videoPreview: string;
+	team: TeamProps[];
+	movementGenres: MovementGenreProps[];
+	workImages: string[];
+	brandColour?: string;
 }
 export interface GenreProps {
-  name: string;
+	name: string;
 }
 
 export interface AwardProps {
-  title: string;
-  year: string;
+	title: string;
+	year: string;
 }
 
 export interface AwardPageProps {
-  name: string;
-  year: string;
-  order: number;
-  work: WorkProps;
+	name: string;
+	year: string;
+	order: number;
+	work: WorkProps;
 }
 
 interface TeamProps {
-  name: string;
-  role: string;
+	name: string;
+	role: string;
 }
 
 interface MovementGenreProps {
-  name: string;
+	name: string;
 }
 
 interface WorkImageProps {
-  asset: {
-    url: string;
-  };
-  alt: string;
+	asset: {
+		url: string;
+	};
+	alt: string;
 }
